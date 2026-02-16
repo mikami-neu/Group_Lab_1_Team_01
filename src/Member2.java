f227af1bc2fafb0ba49d866b81cdc93b6153725a
jwebbnova2026 <baldwin.joh@northeastern.edu>
2026-02-15 21:08:20 -0600

    Implemented Member2 class and added connection to Main.

---
+++ src/Member2.java
@@ -0,0 +1,8 @@
+/**
+ * @author John Baldwin (baldwin.joh@northeastern.edu)
+ */
+public class Member2 {
+  public static void main(String[] args) {
+    System.out.println("Hello from Member 2 (John Baldwin)!");
+  }
+}
